75511c8b6e0dfffb09f261ab542dd65ab79d04fa
musamolla3131 <musamolla3131@gmail.com>
2022-10-12 20:12:09 +0600

    Road map added

--- src/components/Topics/QuizesCard.js
+++ src/components/Topics/QuizesCard.js
@@ -1,6 +1,5 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import Col from 'react-bootstrap/Col';
 const QuizesCard = ({ key, logo, name, total }) => {
 
     return (
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -8,6 +8,11 @@ const Topics = () => {
     const quizedata = quizes.data;
     return (
         <Container>
+            <div className='pt-5 text-start' >
+                <h2>Roadmap to Become a Web Developer in 2022</h2>
+                <p><strong>Web Developer/ Full Stack Web Developer</strong> – How do you feel when you tag yourself with such titles? A long journey takes place to be called by such names. In the beginning, you might feel bored or terrified, but, trust me, this is the most popular and interesting field one should work on. You can also become a web developer provided you know why you want to learn it. According to a study, there are approximately 23 million software developers worldwide and the population growth is going to increase day by day. Now if you don’t know about web development, no worries! All your doubts will get cleared here. </p>
+            </div>
+
             <div className='card-group gap-4 pt-5' >
                 {quizedata.map(quize => <QuizesCard
                     key={quize.id}
