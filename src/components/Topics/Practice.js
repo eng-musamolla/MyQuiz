35c8eca6d130ce07b39633e3df7e787d512f1cbf
musamolla3131 <musamolla3131@gmail.com>
2022-10-12 22:38:54 +0600

    API Data loaded

--- src/components/Topics/Practice.js
+++ src/components/Topics/Practice.js
@@ -1,13 +1,43 @@
+
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import { Container } from 'react-bootstrap';
 
 const Practice = () => {
     const quizes = useLoaderData();
-    console.log(quizes.data);
+    const quiz = quizes.data.questions;
+    console.log(quiz);
+    // const [selectedOption, setSelectedOption] = React.useState();
     return (
-        <div>
-            <h1>Practice Practice Practice</h1>
-        </div>
+        <Container>
+            {
+                quiz.map((question, index) => {
+                    return (
+                        <div>
+                            <div className='d-flex'>
+                                <h4><strong>Quiz {1 + index}:</strong> {question.question}</h4><strong>
+                                </strong>                <p>Answer:{question.correctAnswer}</p>
+                            </div>
+                            <div>
+                                <Form>
+                                    {
+                                        question.options.map(option => {
+                                            return (
+                                                <Form.Check
+                                                    inline label={option} name="group1" type="checkbox"
+                                                    id={`inline-checkbox-1`}
+                                                />
+                                            );
+                                        })
+                                    }
+                                </Form>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </Container>
     );
 };
 
